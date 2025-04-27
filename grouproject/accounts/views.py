9a6299099c55cb4373d9fc6eafa0150dd1474d57
AlfredOfficial <orapaalfred7@gmail.com>
2025-04-27 23:17:27 +0800

    changing this file, huhhuhuh tabang lord saon namn ni, wala nako kasabot

--- grouproject/accounts/views.py
+++ grouproject/accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import AuthenticationForm
 from django.views import View
 from django.contrib.auth.decorators import login_required
+from .models import TimeLog
 
 class CustomLoginView(View):
     def get(self, request):
@@ -18,13 +19,19 @@ class CustomLoginView(View):
             if user.is_superuser:
                 return redirect('/admin/')  # Redirect to admin dashboard
             elif user.is_staff:
-                return redirect('staff_dashboard')
+                return redirect('registration/staff_dashboard')
             else:
                 return redirect('home')
         return render(request, 'registration/login.html', {'form': form})
 
 
-# nag add pero wala pang gana
+# nag add then ni function na pero libog pa
 @login_required
 def staff_dashboard(request):
-    return render(request, 'registration/staff_dashboard.html')
+    if request.user.is_staff:
+        # Fetch the time logs for the logged-in staff member
+        time_logs = TimeLog.objects.filter(staff__staff_username=request.user.username)
+        # pass timelogs to the template
+        return render(request, 'registration/staff_dashboard.html', {'time_logs': time_logs})
+    else:
+        return redirect('home')
