0f6e5a8047f1f7f62d622b5df07343a6f638efdb
Alfred3002 <orapaalfred19@gmail.com>
2025-05-08 11:34:08 -0700

    up

--- grouproject/grouproject/settings.py
+++ grouproject/grouproject/settings.py
@@ -130,5 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/home/'
+LOGIN_REDIRECT_URL = '/admin/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
